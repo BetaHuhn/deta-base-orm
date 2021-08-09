0e89f534a2a9ec27af12777f3880210af14f626d
BetaHuhn <schiller@mxis.ch>
2021-08-09 21:42:42 +0200

    🔖 Release v0.1.1

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,34 +1,174 @@
 import DetaBase from 'deta/dist/types/base';
-export declare type DocumentResponse<Schema> = Document<Schema> & Schema & {
-    /** The ascending key of the document */
+export interface BaseOptions {
+    /** Existing Deta Base instance */
+    db?: DetaBase;
+    /** Generate keys in descending order i.e. newest on top */
+    descending?: boolean;
+    /** Automatically create createdAt field containing the timestamp */
+    timestamp?: boolean;
+}
+/**
+ * The data of a document
+*/
+export declare type BaseDocument<Schema> = Document<Schema> & Schema & {
+    /**
+     * The unique key of the document
+     *
+     * Either auto generated or the one you provided.
+     *
+     * */
     key: string;
-    /**  The unique id of the document */
-    id: string;
-    /** The date when the document was created */
-    addedAt: string;
+    /**
+     * Timestamp of when the document was created
+     *
+     * Note: Only set when timestamp option is true
+     * */
+    createdAt?: number;
 };
+/**
+ * Query to use for finding documents
+*/
+export declare type Query<Schema> = Partial<Schema | {
+    /**
+     * The unique key of the document
+     *
+     * Either auto generated or the one you provided.
+     *
+     * */
+    key?: string;
+    /**
+     * Timestamp of when the document was created
+     *
+     * Note: Only set when timestamp option is true
+     * */
+    createdAt?: number;
+}>;
+interface ParsedOptions {
+    ascending: boolean;
+    timestamp: boolean;
+}
+/**
+ * Create and interact with a Deta Base
+*/
 export declare class Base<Schema> {
     _baseName: string;
     _db: DetaBase;
-    constructor(name: string, db?: DetaBase);
-    create(data: Schema): DocumentResponse<Schema>;
-    save(data: Schema): Promise<DocumentResponse<Schema>>;
+    _opts: ParsedOptions;
+    /**
+     * Create a new Base with the provided name, schema and options
+     * @param name Name of the Base
+     * @param opts Options object
+    */
+    constructor(name: string, opts?: BaseOptions);
+    /**
+     * Create a new document with the provided data based on the Base schema
+     * @param data Object representing the data of the new document
+     * @returns Document
+     */
+    create(data: Schema): BaseDocument<Schema>;
+    /**
+     * Helper function to create and immediately save a new document
+     * @param data Object representing the data of the new document
+     * @returns Document
+     */
+    save(data: Schema): Promise<BaseDocument<Schema>>;
+    /**
+     * Wrapper around the Deta Base SDK fetch method
+     *
+     * Automatically gets all items until the limit or since the last item
+     * @internal
+    */
     _fetch(query?: any, limit?: number, last?: string): Promise<any[]>;
-    find(query?: any, limit?: number, last?: string): Promise<DocumentResponse<Schema>[]>;
-    findOne(query: any): Promise<DocumentResponse<Schema> | undefined>;
-    findById(id: string): Promise<DocumentResponse<Schema> | undefined>;
-    findOneAndUpdate(query: any, data: any): Promise<DocumentResponse<Schema>>;
-    findByIdAndUpdate(id: string, data: any): Promise<DocumentResponse<Schema>>;
-    findByIdAndDelete(id: string): Promise<boolean>;
-    findOneAndDelete(query: any): Promise<boolean>;
+    /**
+     * Find all documents matching the query.
+     *
+     * Use limit and last to paginate the result.
+     *
+     * @param query A query object
+     * @returns Array of Documents
+    */
+    find(query?: Query<Schema>, limit?: number, last?: string): Promise<BaseDocument<Schema>[]>;
+    /**
+     * Find a single document matching the query.
+     *
+     * @param query A query object
+     * @returns Document
+    */
+    findOne(query?: Query<Schema>): Promise<BaseDocument<Schema> | undefined>;
+    /**
+     * Find a single document by its key
+     *
+     * @param key The key of the document
+     * @returns Document
+    */
+    findByKey(key: string): Promise<BaseDocument<Schema> | undefined>;
+    /**
+     * Find a single document matching the query and update it with the provided data.
+     *
+     * @param query A query object
+     * @param data The data to update
+     * @returns Document
+    */
+    findOneAndUpdate(query: Query<Schema> | undefined, data: any): Promise<BaseDocument<Schema>>;
+    /**
+     * Find a single document by its key and update it with the provided data.
+     *
+     * @param key The key of the document
+     * @param data The data to update
+     * @returns Document
+    */
+    findByKeyAndUpdate(key: string, data: any): Promise<BaseDocument<Schema>>;
+    /**
+     * Find a single document by its key and delete it.
+     *
+     * @param key The key of the document
+    */
+    findByKeyAndDelete(key: string): Promise<void>;
+    /**
+     * Find a single document matching the query and delete it.
+     *
+     * @param query A query object
+    */
+    findOneAndDelete(query?: Query<Schema>): Promise<void>;
 }
-export declare class Document<Schema> {
+/**
+ * Represents a Document with all of its data and methods
+ * @internal
+*/
+declare class Document<Schema> {
     [k: string]: any;
     static _baseName: string;
     static _db: DetaBase;
+    static _opts: ParsedOptions;
+    /**
+     * Create a new Document instance with the provided data.
+     *
+     * Will auto generate a key if it is missing.
+     * @internal
+    */
     constructor(data: Schema);
-    update(changes: any): any;
-    delete(): Promise<boolean>;
+    /**
+     * Update the document with the provided data
+     *
+     * @param data The data to update
+    */
+    update(data: any): void;
+    /**
+     * Delete the document
+    */
+    delete(): Promise<void>;
+    /**
+     * Save the Document to the database
+     *
+     * @returns Document
+    */
     save(): Promise<this>;
-    static create<Schema>(data: any): DocumentResponse<Schema>;
+    /**
+     * Create a new Document
+     *
+     * Is used instead of the contructor in order to return the data with a different type, ref: https://git.io/JR2Yc
+     * @internal
+    */
+    static create<Schema>(data: any): BaseDocument<Schema>;
 }
+export {};
--- lib/random.d.ts
+++ lib/random.d.ts
@@ -1,2 +1,10 @@
-export declare const generateKey: (descending: boolean) => string;
-export declare const generateId: () => string;
+/**
+ * Generate a unique key in ascending or descending sequential order
+ *
+ * In ascending mode it will use the Unix timestamp directly
+ * and in descending order the maxDateNowValue (8.64e15) - Unix timestamp
+ * to make sure key is sequential. Key will be timestamp in hex plus a 5 char random id.
+ *
+ * Note: Because of the timestamp in ms, key is only sequential until a certain point i.e two keys generated in the same ms.
+ */
+export declare const generateKey: (ascending: boolean) => string;
