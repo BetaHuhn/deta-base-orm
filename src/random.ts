821db0da6cedcaac06c59c52a9385cfe058fcb5b
BetaHuhn <schiller@mxis.ch>
2021-09-13 22:20:25 +0200

    🚨 Fix lint errrors

--- src/random.ts
+++ src/random.ts
@@ -5,12 +5,14 @@ const maxDateNowValue = 8.64e15 // Fun fact: This will only work until the year 
 
 /**
  * Generate a unique key in ascending or descending sequential order
- * 
+ *
  * In ascending mode it will use the Unix timestamp directly
  * and in descending order the maxDateNowValue (8.64e15) - Unix timestamp
  * to make sure key is sequential. Key will be timestamp in hex plus a 5 char random id.
- * 
+ *
  * Note: Because of the timestamp in ms, key is only sequential until a certain point i.e two keys generated in the same ms.
+ * @param {boolean} ascending
+ * @returns {string} randomKey
  */
 export const generateKey = (ascending: boolean): string => {
 
